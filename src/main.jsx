3ca1a2f30a37bc080057fb0e16b16dc711c879e2
Faraz <mohd.faraz2305@gmail.com>
2026-05-15 21:52:58 +0530

    Metadata and favicon

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { setCustomFavicon } from './utils/favicon.js';
+
+setCustomFavicon();
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
